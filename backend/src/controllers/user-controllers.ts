a702ec8a7c19de68eecedff95fd35bd9a7fcf16d
setouchi-h <setouchi.kazuki@gmail.com>
2023-11-04 16:46:32 +0900

    create: login

--- backend/src/controllers/user-controllers.ts
+++ backend/src/controllers/user-controllers.ts
@@ -1,1 +1,60 @@
-export const getAllUsers = () => {};
+import { NextFunction, Request, Response } from "express";
+import User from "../models/User.js";
+import { hash, compare } from "bcrypt";
+
+export const getAllUsers = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const users = await User.find();
+    return res.status(200).json({ message: "OK", users });
+  } catch (error) {
+    console.log(error);
+    return res.status(200).json({ message: "ERROR", cause: error.message });
+  }
+};
+
+export const userSignup = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const { name, email, password } = req.body;
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      return res.status(401).send("User already exists");
+    }
+    const hashedPassword = await hash(password, 10);
+    const user = new User({ name, email, password: hashedPassword });
+    await user.save();
+    return res.status(201).json({ message: "OK", id: user._id.toString() });
+  } catch (error) {
+    console.log(error);
+    return res.status(200).json({ message: "ERROR", cause: error.message });
+  }
+};
+
+export const userLogin = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(401).send("User does not exist");
+    }
+    const isPasswordCorrect = await compare(password, user.password);
+    if (!isPasswordCorrect) {
+      return res.status(403).send("Incorrect password");
+    }
+    return res.status(200).json({ message: "OK", id: user._id.toString() });
+  } catch (error) {
+    console.log(error);
+    return res.status(200).json({ message: "ERROR", cause: error.message });
+  }
+};
